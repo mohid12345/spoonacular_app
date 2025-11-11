d0b0b97abdfa46e4f64cad6255c872fcbbdeaac4
Mohid Mohan <mohidmohan8482@gmail.com>
2025-11-11 15:31:31 +0530

    hotfix/test-change-5

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
 
 const router = Router();
-const userController = new UserController();
+const userController = new UserController();  
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
