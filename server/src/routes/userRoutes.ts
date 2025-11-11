1d99b913bfd6cb58ef4a324dad3ab33249117286
Mohid Mohan <mohidmohan8482@gmail.com>
2025-11-11 15:43:45 +0530

    git error fix - added userRouter back

---
+++ server/src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/UserController';
+
+const router = Router();
+const userController = new UserController();  
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.post('/refresh',  userController.refreshToken);
+
+export { router as UserRouter };
